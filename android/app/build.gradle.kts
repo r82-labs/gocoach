3b9eb2ce52265f74395b090006678b8b1116bdda
r82labs-ronan <ronan@r82labs.com>
2025-05-30 23:22:23 +0100

    test

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.gocoach"
+    namespace = "com.r82labs.gocoach"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.gocoach"
+        applicationId = "com.r82labs.gocoach"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
